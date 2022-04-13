cc618d281718f56b15f891c4f14073b610ad2429
Neha Koppikar <nehakoppikar@Nehas-MacBook-Pro.local>
2022-04-13 16:09:27 +0530

    remove sensitive info

--- Capstone_PI_Codes/Sem8_Capstone/creds.py
+++ Capstone_PI_Codes/Sem8_Capstone/creds.py
@@ -1,4 +1,3 @@
-#key="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"
-key="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"
-to_email="neha.koppikar@gmail.com"
-from_email="neha.koppikar1721@nmims.edu.in"
+key=""
+to_email=""
+from_email=""
